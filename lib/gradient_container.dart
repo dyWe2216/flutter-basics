6c07b6cd77e37cb6e19bf7e0d7fe501fc0d63320
위대영 <dywe@widaeyeong-ui-MacBookPro.local>
2023-09-13 07:54:24 +0900

    init: project init

---
+++ lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.orange,
+            Colors.deepOrange,
+            Colors.red,
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(inherit: true, color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:basics/gradient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(255, 63, 5, 120),
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
